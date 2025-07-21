f636b017871d754e709704ae66b6b0d3835bb92a
Guangda Liu <bingps@users.noreply.github.com>
2025-07-21 18:38:00 +0800

    fix

--- conf.sh
+++ conf.sh
@@ -1,5 +1,5 @@
 apt update && apt install zsh tmux
-cat > ~/.zshrc << EOF
+cat > ~/.zshrc << 'EOF'
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -48,7 +48,7 @@ bindkey '^R' history-incremental-search-backward
 bindkey -M viins 'jj' vi-cmd-mode
 EOF
 
-cat > ~/.tmux.conf << EOF
+cat > ~/.tmux.conf << 'EOF'
 unbind C-b
 
 set-option -g prefix M-s
